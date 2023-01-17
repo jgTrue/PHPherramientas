fc563bb676a76547432eda511a023b93a2f99123
jgTrue <jgTrue@protonmail.com>
2023-01-17 11:00:49 +0100

    Paso 1

---
+++ Dulces.php
@@ -0,0 +1,54 @@
+<?php 
+
+class dulce{
+
+    private const IVA = 0.21;
+
+    public function __construct(
+        public $nombre,
+        protected $numero,
+        private $precio
+    )
+    {
+        
+    }
+    
+    // Get the value of nombre
+     
+    public function getNombre()
+    {
+            return $this->nombre;
+    }
+
+    // Get the value of numero
+
+    public function getNumero()
+    {
+            return $this->numero;
+    }
+
+
+    // Get the value of precio
+
+    public function getPrecio()
+    {
+            return $this->precio;
+    }
+
+    // Get the value of precio IVA
+
+    public function getPrecioConIVA(){
+        return $this->precio + ($this->precio * self::IVA);
+    }
+
+    public function muestraResumen()
+    {
+        return 'Nombre: ' . $this->getNombre()
+            . '<br>Numero: ' . $this->getNumero()
+            . '<br>Precio: ' . $this->getPrecio()
+            . '€<br>Precio con IVA: ' . $this->getPrecioConIVA() . '€';
+    }   
+     
+}
+
+?>
---
+++ index1.php
@@ -0,0 +1,7 @@
+<?php
+include_once('Dulces.php');
+
+$bollo = new Dulce('bollicao', 35, 1.00);
+
+echo $bollo->muestraResumen();
+?>  
